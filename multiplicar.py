b0c0649b52ba422c8eefa3f8a8405880d357dabf
Your Name <you@example.com>
2026-01-30 12:07:34 +0100

    actualizacion 2

---
+++ multiplicar.py
@@ -0,0 +1,6 @@
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+resultado = num1 * num2
+
+print("El resultado de la multiplicación es:", resultado)
